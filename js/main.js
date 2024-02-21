e0063ab5b200ab894542c442a13d6fae9ed7aa7d
wongb <WongBaoTEch@outlook.com>
2024-02-21 13:21:45 +1200

    build navigation component

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+/* front end user interacts with toggler by 'click' */
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
